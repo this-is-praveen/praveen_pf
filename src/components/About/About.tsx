5dd20eabde8de770d2387f911edfc4b897981192
Praveen <gpraveenin@gmail.com>
2022-11-06 09:10:02 +0530

    About content update

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -21,83 +21,79 @@ export const About = () => {
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000}>
           <p>
-            Howdy, I computer science engineer with a great passion for
-            programming. I am passionate about delivering solutions that add to
-            people's lives and at the same time challenge me. Improved my skills
-            as a Front-End and Back-End developer
+            Howdy, I computer science building with great passion for
+            programming. I am enthusiastic about conveying arrangement and
+            simultaneously challenge me. Made strides in my skills as a
+            Front-End and Back-End designer
           </p>
         </ScrollAnimation>
-        <ScrollAnimation
-          animateIn="fadeInLeft"
-          delay={0.4 * 1000}
-          style={{ marginTop: "2rem", marginBottom: "2rem" }}
-        >
+        <br />
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000}>
           <p>
-            I develop websites and applications and I always improving myself
-            with each project they put in my hands.
-          </p>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.6 * 1000}>
-          <p>
-            I am a dedicated person who pursues my dreams, hardworking and
-            results-oriented, I always seek to achieve my best version.
+            I am a devoted individual who seeks after my dreams, dedicated and
+            results-oriented, I continuously look to realize my best adaptation.
           </p>
         </ScrollAnimation>
 
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.7 * 1000}>
-          <h3>Primary Skills:</h3>
+        <br />
+        <br />
+
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.5 * 1000}>
+          <h3>Skills:</h3>
         </ScrollAnimation>
-        <div className="hard-skills">
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.1 * 1000}>
-              <img src={reactIcon} alt="React" />
-            </ScrollAnimation>
-          </div>
+        <ScrollAnimation animateIn="shake" delay={1000} initiallyVisible={true}>
+          <div className="hard-skills">
+            <div className="hability">
+              <ScrollAnimation animateIn="fadeInUp" delay={0.1 * 1000}>
+                <img src={reactIcon} alt="React" />
+              </ScrollAnimation>
+            </div>
 
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.2 * 1000}>
-              <img src={jsIcon} alt="JavaScript" />
-            </ScrollAnimation>
-          </div>
+            <div className="hability">
+              <ScrollAnimation animateIn="fadeInUp" delay={0.2 * 1000}>
+                <img src={jsIcon} alt="JavaScript" />
+              </ScrollAnimation>
+            </div>
 
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.3 * 1000}>
-              <img src={htmlIcon} alt="Html" />
-            </ScrollAnimation>
-          </div>
+            <div className="hability">
+              <ScrollAnimation animateIn="fadeInUp" delay={0.3 * 1000}>
+                <img src={htmlIcon} alt="Html" />
+              </ScrollAnimation>
+            </div>
 
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.3 * 1000}>
-              <img src={cssIcon} alt="Css" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.5 * 1000}>
-              <img src={nodeIcon} alt="Node" />
-            </ScrollAnimation>
-          </div>
+            <div className="hability">
+              <ScrollAnimation animateIn="fadeInUp" delay={0.3 * 1000}>
+                <img src={cssIcon} alt="Css" />
+              </ScrollAnimation>
+            </div>
+            <div className="hability">
+              <ScrollAnimation animateIn="fadeInUp" delay={0.5 * 1000}>
+                <img src={nodeIcon} alt="Node" />
+              </ScrollAnimation>
+            </div>
 
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.6 * 1000}>
-              <img src={typescriptIcon} alt="Typescript" />
-            </ScrollAnimation>
+            <div className="hability">
+              <ScrollAnimation animateIn="fadeInUp" delay={0.6 * 1000}>
+                <img src={typescriptIcon} alt="Typescript" />
+              </ScrollAnimation>
+            </div>
+            <div className="hability">
+              <ScrollAnimation animateIn="fadeInUp" delay={0.7 * 1000}>
+                <img src={gqlIcon} alt="Graph QL" />
+              </ScrollAnimation>
+            </div>
+            <div className="hability">
+              <ScrollAnimation animateIn="fadeInUp" delay={0.8 * 1000}>
+                <img src={mysqlIcon} alt="MySQL" />
+              </ScrollAnimation>
+            </div>
+            <div className="hability">
+              <ScrollAnimation animateIn="fadeInUp" delay={0.9 * 1000}>
+                <img src={mongoIcon} alt="Mongo DB" />
+              </ScrollAnimation>
+            </div>
           </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.7 * 1000}>
-              <img src={gqlIcon} alt="Graph QL" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.8 * 1000}>
-              <img src={mysqlIcon} alt="MySQL" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.9 * 1000}>
-              <img src={mongoIcon} alt="Mongo DB" />
-            </ScrollAnimation>
-          </div>
-        </div>
+        </ScrollAnimation>
       </div>
       <div className="about-image">
         <ScrollAnimation
