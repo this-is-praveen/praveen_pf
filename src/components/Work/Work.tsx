5ecf322a265305fd70f3b95caefb3fb74ea707ad
Prawin G <gpraveenin@gmail.com>
2022-12-20 12:08:09 +0530

    Update Work.tsx
    Added Scenic Part

--- src/components/Work/Work.tsx
+++ src/components/Work/Work.tsx
@@ -217,6 +217,56 @@ export function Work() {
                 <title>Folder</title>
                 <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
               </svg>
+              <div className="project-links">
+                <a
+                  href="https://github.com/this-is-praveen/next-js-scenario"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={githubIcon} alt="GitHub" />
+                </a>
+                <a
+                  href="https://scenic.vercel.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={externalLinkIcon} alt="Scenic Site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Scenic</h3>
+              <p>
+                The Site that built with Next JS and the page is rendered with Static Site Generation
+                You can create and post any scenario's
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Next JS</li>
+                <li>CSS Module</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b "
+                strokeWidth="1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
 
               <div className="project-links">
                 <a
